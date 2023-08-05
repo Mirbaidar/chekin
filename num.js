4a4cde5f0175439755c822f8a89901faac49954c
BAIDAR BAKHAT <mirbaidarmirbaidar@gmail.com>
2023-08-05 20:43:31 +0530

    Initial commit

---
+++ num.js
@@ -0,0 +1,103 @@
+/*let zn=document.getElementById("keep");
+let heet=document.getElementById("hit");
+let cd=document.createElement("span");
+let cnn=document.getElementById("name");
+let is=0;
+let student=["baidar","wahid","uzair","faizan"];
+cd.innerHTML="<h1> u are in team and  playing 11 </h1>"
+heet.addEventListener("click",change);
+
+function change(){
+    let val=cnn.value;
+    
+    for(let i=0;i<student.length;i++){
+        if(val==student[i]){
+            is=1;
+            zn.append(cd);
+            break;
+        }
+    }
+    if (is==0) {
+        cd.innerHTML="<h1> sorry u are not in playing 11 </h1>"
+        zn.append(cd);
+        
+    }
+}
+
+let people = {
+  name: "king",
+  age: 3,
+  classname: "king",
+  rollno: 3,
+
+  fan:function(){
+    console.log("how are u man");
+  }
+};
+people.fan();
+for(let key in people){
+    console.log(key +":"+ people[key]);
+}
+*/
+
+
+/*let xahar={
+    man :"baidar",
+    roll :20,
+    age : 15,
+    schoool :"M.P.s janwara",
+   hello : function(){
+        console.log("u are trying great things");
+    }
+};
+
+
+xahar.keep = "sopore";
+
+console.log(xahar.keep);
+for( king in xahar){
+    console.log(king);
+}
+xahar.hello();*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
